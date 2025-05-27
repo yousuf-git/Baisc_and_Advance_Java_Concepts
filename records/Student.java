0b44486112eb28a616edd121284cd7a52ce87569
yousuf-git <hackerbest052@gmail.com>
2025-05-27 19:23:14 +0500

    add few more details about records

--- records/Student.java
+++ records/Student.java
@@ -4,8 +4,12 @@
  * Things to know about records:
  * - Records are immutable
  * - Records automatically generate equals(), hashCode(), and toString() methods
+ * - Records cannot extend other classes (they implicitly extend java.lang.Record)
+ * - Records implicitly creates final fields for each component passed in the constructor
+ * - Automatically generates a constructor that takes all components as parameters
+ * - Automatically generates accessor methods for each component, For example, for a record `Student`, it will generate `rollNo()`, `name()`, and `email()` methods that can be referred to as getters
  * - Records can implement interfaces
- * - Records can have a compact constructor
+ * - Records can have a compact constructor (sometimes used for validation stuff)
  *      Example:
  *       ```
  *       public record Student(String rollNo, String name, String email) {
